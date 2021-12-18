9794f6371773f82b60a8c93be9106ebb80f49b28
gouravmarch20 <gouravmarch20@gmail.com>
2021-12-18 18:44:53 +0530

    backend : mvc created

--- server/index.js
+++ server/index.js
@@ -15,9 +15,9 @@ const postRoutes = require('./routes/posts');
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+app.use(bodyParser.json({ limit: '30mb', extended: true }))// limit photo size
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))//
+app.use(cors());//
 
 app.use('/posts', postRoutes);
 
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { home } = require("../controllers/postController");
+const { getPosts, getPost, createPost, updatePost, likePost, deletePost } = require("../controllers/postController");
 
 
-router.route("/").get(home);
+router.get('/', getPosts);
+router.post('/', createPost);
+router.get('/:id', getPost);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
 
 module.exports = router;
